03e594b0062ac649964b9a1a84e252e1ed1ca954
PlayRobotics <60300961+playrobotics-alex@users.noreply.github.com>
2022-04-20 10:58:37 +0300

    iox-fix2

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -489,15 +489,15 @@ playSingleLock() {
           //if(isLogEnabled) console.log("-==subscribing==-");
           console.log("-==subscribing==-");
           this.SubscribedToNotifyBLE = true;
-          this.ble.startNotification(this.trainID, TRAINER_SERVICE_UUID, "7E400003-B5A3-F393-E0A9-E50E24DCCA9E", this.onNotify);
-          /*
+         // this.ble.startNotification(this.trainID, TRAINER_SERVICE_UUID, "7E400003-B5A3-F393-E0A9-E50E24DCCA9E", onNotify);
+ 
           this.ble.startNotification(this.trainID, TRAINER_SERVICE_UUID, "7E400003-B5A3-F393-E0A9-E50E24DCCA9E").subscribe(
             (data) => {
               this.onNotify(data);              
             },
             (error) => console.error('Unexpected Error', 'Failed to subscribe')
           )
-          */
+  
           
         }
         this.storage.get("IndoorLightsToggle").then((value) => {
